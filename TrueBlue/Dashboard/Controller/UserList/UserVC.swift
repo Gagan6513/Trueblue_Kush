81f34b33fa8dd0cb43bb44804533966bf3a3808c
Janak Jivrani <janak.jivrani@gmail.com>
2024-01-09 12:36:55 +0530

    search functionality implemented in all user

--- TrueBlue/Dashboard/Controller/UserList/UserVC.swift
+++ TrueBlue/Dashboard/Controller/UserList/UserVC.swift
@@ -10,10 +10,13 @@ import UIKit
 class UserVC: UIViewController {
 
 //    MARK: - Outlet
+    @IBOutlet weak var searchTextField: UITextField!
     @IBOutlet weak var tableView: UITableView!
     
 //    MARK: - Variable
     var allUserData = AllUserModel()
+    var filterData = [AllUserListData]()
+    var search = ""
     
 //    MARK: - Override Function
     override func viewDidLoad() {
@@ -66,9 +69,9 @@ extension UserVC: UITableViewDelegate, UITableViewDataSource {
         webService.url = URL(string: API_URL.force_logout_user)!
         webService.method = .post
         var param = [String: String]()
-        param["userId"] = data.username ?? ""
+        param["userId"] = data.id ?? ""
         param["requestFrom"] = request_from
-        param["createdBy"] = "1"
+        param["createdBy"] = UserDefaults.standard.userId()
         param["isForceLogout"] = (data.LOGOUT_ID ?? "").lowercased() == "0" ? "1" : "0"
         webService.parameters = param
         
@@ -112,7 +115,32 @@ extension UserVC {
         self.tableView.registerNib(for: "UserListTVC")
         self.tableView.contentInset = UIEdgeInsets(top: 10, left: 0, bottom: 0, right: 0)
         
+        self.searchTextField.delegate = self
+        
         self.getUserData()
+    }
+}
+
+extension UserVC: UITextFieldDelegate {
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        
+        search = string.isEmpty ? String(search.dropLast()) : textField.text!+string
+        
+        self.allUserData.data?.response = self.filterData
+        
+        self.allUserData.data?.response = self.allUserData.data?.response?.filter{$0.username?.lowercased().contains(search.lowercased()) ?? false}
+        
+        if search == "" {
+            self.allUserData.data?.response = self.filterData
+        }
+        self.tableView.reloadData()
+        return true
+    }
+    
+    func textFieldShouldClear(_ textField: UITextField) -> Bool {
+        self.allUserData.data?.response = self.filterData
+        self.tableView.reloadData()
+        return true
     }
 }
 
@@ -152,6 +180,7 @@ extension UserVC {
                     }
                     
                     self.allUserData = data
+                    self.filterData = data.data?.response ?? []
                     
                     self.tableView.reloadData()
                 }
