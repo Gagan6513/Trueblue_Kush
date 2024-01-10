d6cbad706ebf2b83b2ddf1ff655c69f907a11aab
Janak Jivrani <janak.jivrani@gmail.com>
2024-01-10 13:26:20 +0530

    Design changes in upcoming booking and fixed search functionality

---
+++ TrueBlue/Dashboard/Controller/UpComingBookingList/TVC/UpcomingBookingTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  UpcomingBookingTableViewCell.swift
+//  TrueBlue
+//
+//  Created by Kushkumar Katira iMac on 10/01/24.
+//
+
+import UIKit
+
+class UpcomingBookingTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
