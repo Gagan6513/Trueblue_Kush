81dec6da10e42881b9c15afaf7c737a4af5d9578
Janak Jivrani <janak.jivrani@gmail.com>
2024-01-11 11:10:28 +0530

    UIChanges in event

---
+++ TrueBlue/Dashboard/Controller/Event/TVC/NewEventTVC/NewEventTVC.swift
@@ -0,0 +1,23 @@
+//
+//  NewEventTVC.swift
+//  TrueBlue
+//
+//  Created by Kushkumar Katira iMac on 10/01/24.
+//
+
+import UIKit
+
+class NewEventTVC: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
